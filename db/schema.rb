c0cdfb7d990935ca4bbd4a3f0a17fa118a9ef01e
Trey Fletcher <treyfletcher612@gmail.com>
2018-09-25 17:52:42 +0000

    added concerns and defaults

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_145033) do
+ActiveRecord::Schema.define(version: 2018_09_25_174153) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_145033) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
