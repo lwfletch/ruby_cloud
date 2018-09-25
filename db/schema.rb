53f56711d7182ed96d94f0c12af47b92f5f5d00b
Trey Fletcher <treyfletcher612@gmail.com>
2018-09-25 14:56:12 +0000

    created topics, added validations, ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_225608) do
+ActiveRecord::Schema.define(version: 2018_09_25_145033) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 2018_09_24_225608) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -45,6 +47,12 @@ ActiveRecord::Schema.define(version: 2018_09_24_225608) do
   end
 
   create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
